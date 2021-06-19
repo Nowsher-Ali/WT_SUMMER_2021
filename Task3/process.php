74590b40953c73853f1518702d831fffaef3bb2d
Nowsher-Ali <80275914+Nowsher-Ali@users.noreply.github.com>
2021-06-19 11:39:11 +0600

    Task3

--- Task3/process.php
+++ Task3/process.php
@@ -11,7 +11,7 @@
            'File Path' => $target_file,
 		   'Hobby 1' => $_POST['Hobby1'],
 		   'Hobby 2' => $_POST['Hobby2'],
-		   'Hobby 3' => $_POST['Hobby3'],
+		   'Hobby 3' => $_POST['Hobby3']
 	   );
        $existingdata = file_get_contents('data.json');
        $tempJSONdata = json_decode($existingdata);
